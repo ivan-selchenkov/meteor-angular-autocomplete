ff058e50346ed494a2a68ce9bbe5e9b0dc6f5341
Ivan Selchenkov <vashik.net@gmail.com>
2014-11-29 07:30:28 +0300

    init

---
+++ package.js
@@ -0,0 +1,23 @@
+Package.describe({
+  name: 'selchenkov:angular-autocomplete',
+  summary: 'Meteor package for the angular-autocomplete library',
+  version: '0.0.1',
+  git: 'https://github.com/vashik/meteor-angular-autocomplete.git'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.use([
+        'urigo:angular@0.5.5',
+        'mquandalle:bower@0.1.11',
+        'mvrx:bluebird@0.0.1',
+        'stevezhu:lodash@1.0.2'],
+      'client');  // Add bower
+  api.addFiles('selchenkov:angular-autocomplete.js');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('selchenkov:angular-autocomplete');
+  api.addFiles('selchenkov:angular-autocomplete-tests.js');
+});
